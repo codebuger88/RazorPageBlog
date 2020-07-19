5afd2987a4b492e9403fea48ac3acc6b0c1ba586
Kyle <nagual204@gmail.com>
2020-07-19 19:24:15 +0800

    管理界面layout及管理文章列表

---
+++ Web/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Web.Data;
+using Web.Data.Models;
+
+namespace Web.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPageBlogContext _context;
+
+        public IndexModel(RazorPageBlogContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Article> Articles { get; set; }
+
+
+        public async Task OnGetAsync()
+        {
+            Articles = await _context.Article.ToListAsync();
+        }
+    }
+}
